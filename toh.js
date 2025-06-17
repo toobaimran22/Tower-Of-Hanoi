ca4e785b5f6c660e64125808562b2ffa65dd5fcf
deltalabsMac <deltalabsmac@192.168.10.9>
2025-06-17 15:02:03 +0500

    Improved disk generation and cleaned up layout logic
    Improved disk generation and cleaned up layout logic

--- toh.js
+++ toh.js
@@ -1,39 +1,32 @@
 $(function () {
   let moveCount = 0;
 
-  const diskColors = [
-    "#e74c3c", "#d35400", "#f39c12", "#27ae60",
-    "#2980b9", "#8e44ad", "#2c3e50", "#7f8c8d"
-  ];
-
   function generateDisks(n) {
-    const $tower1 = $("#tower1");
     $(".disk").remove(); 
     for (let i = n; i >= 1; i--) {
-      const $disk = $("<div>")
+      const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+      $("<div>")
         .addClass("disk")
         .attr("id", "disk" + i)
         .text(i)
         .css({
           width: 40 + i * 15 + "px",
-          backgroundColor: diskColors[i - 1]
-        });
-      $tower1.append($disk);
+          backgroundColor: randomColor
+        })
+        .appendTo($("#tower1"));
     }
   }
 
   function makeDraggable() {
-    $(".tower").each(function () {
-      const $topDisk = $(this).children(".disk").last();
-      if ($topDisk.length > 0) {
-        $topDisk.draggable({
-          revert: "invalid",
-          containment: ".container",
-          zIndex: 1000
-        });
-      }
+  $(".tower").each(function () {
+    $(this).children(".disk").last().draggable({
+      revert: "invalid",
+      containment: ".container",
+      zIndex: 1000
     });
-  }
+  });
+}
+
 
   function setupDroppables() {
     $(".tower").droppable({
@@ -53,27 +46,23 @@ $(function () {
   }
 
   function checkWin() {
-    const totalDisks = parseInt($("#diskCount").val());
-    const tower2Disks = $("#tower2").children(".disk").length;
-    const tower3Disks = $("#tower3").children(".disk").length;
-
-    if (tower2Disks === totalDisks || tower3Disks === totalDisks) {
+    if ([$("#tower2 .disk").length, $("#tower3 .disk").length].includes(+$("#diskCount").val())) {
       setTimeout(() => {
-        alert("Congratulations! You solved the Tower of Hanoi in " + moveCount + " moves!");
+        alert(`Congratulations! You solved the Tower of Hanoi in ${moveCount} moves!`);
       }, 200);
     }
   }
 
+
   function resetGame() {
     moveCount = 0;
     $("#moveCounter").text("Moves: " + moveCount);
-    const diskCount = parseInt($("#diskCount").val());
+    const diskCount = +$("#diskCount").val();
     generateDisks(diskCount);
     makeDraggable();
   }
 
   $("#resetBtn").click(resetGame);
-
   $("#diskCount").change(resetGame);
 
   resetGame();
